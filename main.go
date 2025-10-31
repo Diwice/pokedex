a35d3474ece0debc96150476e270bc89ee0f4e6f
Diwice <coldwice@gmail.com>
2025-11-01 02:38:52 +0300

    CH1L5

--- main.go
+++ main.go
@@ -7,15 +7,54 @@ import (
 	"dep/clinput"
 )
 
+type cli_command struct {
+	name	    string
+	description string
+	callback    func() error
+}
+
+func get_cmds() map[string]cli_command {
+	return map[string]cli_command{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    command_exit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    command_help,
+		},
+	}
+
+}
+
+func command_exit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return fmt.Errorf("Failed to exit")
+}
+
+func command_help() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n")
+
+	for _, v := range get_cmds() {
+		fmt.Printf("%s: %s\n", v.name, v.description)
+	}
+
+	return nil
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Print("Pokedex > ")
 
 		scanner.Scan()
 
 		if err := scanner.Err(); err != nil {
-			fmt.Println("Encountered an error", err)
+			fmt.Println("Encountered an error during read of Stdin", err)
 			continue
 		}
 
@@ -26,6 +65,15 @@ func main() {
 			continue
 		}
 
-		fmt.Printf("Your command was: %s\n", inp[0])
+		e, ok := get_cmds()[inp[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+
+			continue
+		}
+
+		if err := e.callback(); err != nil {
+			fmt.Println(err)
+		}
 	}
 }
