050b0e909968a2aac7c87e35d60f7b4339fbb88d
Diwice <coldwice@gmail.com>
2025-11-01 02:06:19 +0300

    CH1L4

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"os"
 	"fmt"
+	"bufio"
 	"dep/clinput"
 )
 
 func main() {
-	em_string := clinput.Clean_Input("lol")
-	fmt.Print(em_string)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Encountered an error", err)
+			continue
+		}
+
+		inp := clinput.Clean_Input(scanner.Text())
+
+		if len(inp) == 0 {
+			fmt.Println("You must input anything")
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", inp[0])
+	}
 }
