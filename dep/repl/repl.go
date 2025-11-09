56b231c956f3f4e8ab07b7d99ff05f14993696df
Diwice <coldwice@gmail.com>
2025-11-09 15:47:49 +0300

    Catch command

--- dep/repl/repl.go
+++ dep/repl/repl.go
@@ -3,8 +3,10 @@ package repl
 import (
 	"os"
 	"fmt"
+	"time"
 	"net/http"
 	"dep/cache"
+	"math/rand"
 	"encoding/json"
 )
 
@@ -12,6 +14,7 @@ type Cfg struct {
 	next 	 string
 	previous string
 	Ch       *cache.Cache
+	pokedex  map[string]pokemon
 }
 
 type cli_command struct {
@@ -33,7 +36,8 @@ type map_struct struct {
 }
 
 type pokemon struct {
-	Name string `json:"name"`
+	Name     string `json:"name"`
+	Base_exp int    `json:"base_experience"`
 }
 
 type poke_encounter struct {
@@ -48,28 +52,33 @@ func Get_cmds() map[string]cli_command {
 	return map[string]cli_command{
 		"exit": {
 			name:        "exit",
-			description: "Exit the Pokedex",
+			description: "Exit the Pokedex.",
 			Callback:    command_exit,
 		},
 		"help": {
 			name:        "help",
-			description: "Displays a help message",
+			description: "Displays a help message.",
 			Callback:    command_help,
 		},
 		"map": {
 			name:        "map",
-			description: "Shows next page available locations",
+			description: "Shows next page available locations.",
 			Callback:    command_map,
 		},
 		"mapb": {
 			name:        "mapb",
-			description: "Shows previous page of available locations",
+			description: "Shows previous page of available locations.",
 			Callback:    command_mapb,
 		},
 		"explore": {
 			name:        "explore",
-			description: "Shows the Pokemons inhabiting the named area",
+			description: "Shows the Pokemons inhabiting the named area. explore <area-name>",
 			Callback:    command_explore,
+		},
+		"catch": {
+			name:        "catch",
+			description: "Attempt to catch a named pokemon. catch <pokemon-name>",
+			Callback:    command_catch,
 		},
 	}
 
@@ -205,8 +214,6 @@ func command_mapb(c *Cfg, params string) error {
 }
 
 func command_explore(c *Cfg, params string) error {
-	fmt.Printf("Exploring %s...\n", params)
-
 	sub_url := fmt.Sprintf("https://pokeapi.co/api/v2/location-area/%s", params)
 
 	entry, ok := c.Ch.Get(sub_url)
@@ -236,6 +243,8 @@ func command_explore(c *Cfg, params string) error {
 		return err
 	}
 
+	fmt.Printf("Exploring %s...\n", params)
+
 	res := ""
 	for _, enc := range poke_encs.Encounters {
 		res += fmt.Sprintf(" - %s\n", enc.Pokemon.Name)
@@ -244,6 +253,51 @@ func command_explore(c *Cfg, params string) error {
 	fmt.Print(res)
 
 	c.Ch.Add(sub_url, []byte(res))
+
+	return nil
+}
+
+func command_catch(c *Cfg, params string) error {
+	const base_modifier = 7.0
+
+	sub_url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s/", params)
+
+	req, err := http.Get(sub_url)
+	if err != nil {
+		return err
+	}
+
+	if req.StatusCode == 404 {
+		return fmt.Errorf("Pokemon with this name doesn't exist!")
+	} else if req.StatusCode > 299 {
+		return fmt.Errorf("Request failed, status code: %d", req.StatusCode)
+	}
+
+	defer req.Body.Close()
+
+	fmt.Printf("Throwing a Pokeball at %s...\n", params)
+
+	var res pokemon
+	decoder := json.NewDecoder(req.Body)
+	if err := decoder.Decode(&res); err != nil {
+		return err
+	}
+
+	rand_src := rand.New(rand.NewSource(time.Now().UnixNano()))
+	calc_chance := (608.0/float32(res.Base_exp))*base_modifier
+	if rand_src.Float32()*100.0 <= calc_chance {
+		fmt.Printf("%s was caught!\n", params)
+
+		if c.pokedex == nil {
+			c.pokedex = make(map[string]pokemon)
+		}
+
+		c.pokedex[params] = res
+
+		return nil
+	}
+
+	fmt.Printf("%s escaped!\n", params)
 
 	return nil
 }
